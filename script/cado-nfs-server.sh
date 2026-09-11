bf51bd52e8a7c3b3e8fb6f91dff37306dfc1dce8
Vis Virial <visvirial@gmail.com>
2026-09-11 18:28:00 +0900

    Add CADO_NFS_SERVER_OPTS env var.

--- script/cado-nfs-server.sh
+++ script/cado-nfs-server.sh
@@ -12,4 +12,4 @@ fi
 
 N=$(cat "$ROOT/integers/$1.txt")
 
-$CADO_NFS_ROOT/cado-nfs.py $N tasks.workdir=$CADO_NFS_WORKDIR_ROOT/$1 server.port=${CADO_NFS_PORT} server.whitelist=0.0.0.0/0 --server
+${CADO_NFS_ROOT}/cado-nfs.py $N tasks.workdir=${CADO_NFS_WORKDIR_ROOT}/$1 server.port=${CADO_NFS_PORT} server.whitelist=0.0.0.0/0 ${CADO_NFS_SERVER_OPTS} --server
