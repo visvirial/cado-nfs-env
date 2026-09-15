b646ef86dc479d3e0632609746386841144c0906
Vis Virial <visvirial@gmail.com>
2026-09-16 06:48:31 +0900

    Add Dockerfile.

--- script/cado-nfs-client.sh
+++ script/cado-nfs-client.sh
@@ -5,13 +5,9 @@ set -e
 ROOT=$(cd "$(dirname $0)/.." && pwd)
 source "$ROOT/env.sh"
 
-threads=$(grep -F 'cpu cores' /proc/cpuinfo | sort -u | sed 's/.*: //')
-
-echo "Running $threads processes..."
-
 trap "kill -KILL 0" EXIT
 
-for i in `seq $threads`; do
+for i in `seq $CADO_NFS_CLIENT_THREADS`; do
 	$CADO_NFS_ROOT/cado-nfs-client.py --bindir $(cd $CADO_NFS_ROOT; eval `make show` ; echo $build_tree) --server=${CADO_NFS_SERVER} --certsha1=${CADO_NFS_CERTSHA1} &
 done
 
